41b2cafd33ed7366467679593fa105a7050fbbe2
GastonGit <gastonbjorkskog@hotmail.com>
2021-03-11 16:12:36 +0100

    add functionality for adding clips to cliplist

--- server/lib/ClipList.js
+++ server/lib/ClipList.js
@@ -10,6 +10,31 @@ class ClipList {
     getList(){
         return this.currentList;
     }
+
+    addClip(clip){
+        if (typeof clip === 'string'){
+            if (isTwitchClipUrl(clip)){
+                this.currentList.push(clip);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+function isTwitchClipUrl(string){
+
+    if (string.includes("https://clips.twitch.tv/") && string.indexOf("https://") === 0){
+
+        let url = new URL(string);
+
+        if (url.pathname.length > 1){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 let cl = new ClipList();
--- server/test/testClipList.js
+++ server/test/testClipList.js
@@ -1,11 +1,48 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect
 
 let ClipList = require('../lib/ClipList');
 
 describe('ClipList methods', function() {
-    describe('getList', function() {
+    describe('Getting the list', function() {
         it('should return an array', function() {
             assert.isArray(ClipList.getList());
         });
     });
+    describe('Adding clips', function(){
+        it('should return true when adding a string containing a twitch clip url', function() {
+            expect(ClipList.addClip("https://clips.twitch.tv/HealthyDelightfulEchidnaKappaPride")).to.be.true;
+        });
+        it('should return false when adding a string that is not a twitch clip url', function() {
+            expect(ClipList.addClip("https://www.test.com/testing")).to.be.false;
+            expect(ClipList.addClip("eb5+ 9uy349qyb5v97f15")).to.be.false;
+            expect(ClipList.addClip("https://byv04um3608406304/.com/.")).to.be.false;
+            expect(ClipList.addClip("https://www.test.com")).to.be.false;
+            expect(ClipList.addClip("www.test.com/testing")).to.be.false;
+            expect(ClipList.addClip("https://www.clips.test.com/testing")).to.be.false;
+            expect(ClipList.addClip("https://www.clips.twitch.tv/")).to.be.false;
+            expect(ClipList.addClip("https://clips.twitch.tv/")).to.be.false;
+        });
+        it('should return false when adding an integer', function() {
+            expect(ClipList.addClip(20)).to.be.false;
+            expect(ClipList.addClip(0)).to.be.false;
+            expect(ClipList.addClip(-1)).to.be.false;
+            expect(ClipList.addClip(1000000000)).to.be.false;
+            expect(ClipList.addClip(94489098498020942)).to.be.false;
+            expect(ClipList.addClip(-94489098498020942)).to.be.false;
+        });
+        it('should return false when adding an undefined value', function() {
+            expect(ClipList.addClip(undefined)).to.be.false;
+        });
+        it('should not add clip to list when addClip returns false', function() {
+            const string1 = "https://www.test.com/testing";
+            expect(ClipList.addClip(string1)).to.be.false;
+            expect(ClipList.getList()).to.not.include(string1);
+        });
+        it('should add clip to list when addClip returns true', function() {
+            const string1 = "https://clips.twitch.tv/HealthyDelightfulEchidnaKappaPride";
+            expect(ClipList.addClip(string1)).to.be.true;
+            expect(ClipList.getList()).to.include(string1);
+        });
+    })
 });

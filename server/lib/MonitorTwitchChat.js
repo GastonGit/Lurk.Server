8efac236a8513b517aa27946b7b8dbe059877e0d
GastonGit <gastonbjorkskog@hotmail.com>
2021-03-15 16:50:48 +0100

    add methods for setting and getting stream list for monitor

--- server/lib/MonitorTwitchChat.js
+++ server/lib/MonitorTwitchChat.js
@@ -1,8 +1,35 @@
+const tmi = require('tmi.js');
 
 class MonitorTwitchChat{
 
+    streamList;
+
     constructor() {
 
+        this.streamList = [];
+    }
+
+    getStreamList(){
+        return this.streamList;
+    }
+
+    updateStreamList(response){
+        if (typeof response === "undefined"){
+            throw new Error("Argument is undefined");
+        }
+
+        let streamList = this.streamList;
+
+        response["data"].forEach(function(channel){
+            streamList.push({
+                user_name: channel.user_name,
+                viewer_count: channel.viewer_count
+            })
+        })
+    }
+
+    requestStreams(){
+        return {};
     }
 }
 
--- server/test/testMonitorTwitchChat.js
+++ server/test/testMonitorTwitchChat.js
@@ -1,11 +1,53 @@
-const assert = require('chai').assert;
-const expect = require('chai').expect
+const chai = require('chai')
+const spies = require('chai-spies');
+chai.use(spies);
+const assert = chai.assert;
+const expect = chai.expect
+const should = chai.should()
 
 let MonitorTwitchChatClass = require('../lib/MonitorTwitchChat');
 let MonitorTwitchChat;
 
-describe('HotClipsController methods', function() {
-    before(function (){
+describe('MonitorTwitchChat methods', function() {
+    beforeEach(function (){
         MonitorTwitchChat = new MonitorTwitchChatClass();
     })
+    describe('getStreamList', function() {
+        it('should return an array', function() {
+            assert.isArray(MonitorTwitchChat.getStreamList());
+        });
+        it('should be an empty on init', function() {
+            expect(MonitorTwitchChat.getStreamList()).to.be.empty;
+        });
+    });
+    describe('requestStreams', function() {
+        it('should return an object', function() {
+            expect(MonitorTwitchChat.requestStreams()).to.be.an('object');
+        });
+    });
+    describe('updateStreamList', function() {
+        it('should throw if used with undefined argument', function() {
+            expect(MonitorTwitchChat.updateStreamList).to.throw();
+        });
+        it('should update streamList if used with valid object argument', function() {
+            expect(MonitorTwitchChat.getStreamList()).to.be.empty;
+
+            MonitorTwitchChat.updateStreamList({
+                "data": [
+                    {
+                        "user_name": "moonmoon",
+                        "viewer_count": 69
+                    }
+                ]
+            })
+
+            expect(MonitorTwitchChat.getStreamList()).to.deep.include(
+                {
+                "user_name": "moonmoon",
+                "viewer_count": 69
+                }
+            )
+        });
+
+    });
 });

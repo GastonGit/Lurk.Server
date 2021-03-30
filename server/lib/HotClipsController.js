c3301ff603982a0d2636a31775438fcec713728f
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-03-30 20:32:43 +0200

    init instance of MonitorTwitchChat

--- server/lib/HotClipsController.js
+++ server/lib/HotClipsController.js
@@ -1,11 +1,20 @@
 
 let ClipList = require("./ClipList");
+let MonitorTwitchChat = require('./MonitorTwitchChat');
+let TwitchClient = require('./TwitchClient');
 
 class HotClipsController{
 
     clipList;
+    monitorTwitchChat;
 
     constructor() {
+        this.monitorTwitchChat = new MonitorTwitchChat(
+            new TwitchClient(),
+            {
+            requestCount: undefined,
+            validMessages: undefined
+        });
         this.clipList = new ClipList();
     }
 
--- server/test/testHotClipsController.js
+++ server/test/testHotClipsController.js
@@ -1,13 +1,24 @@
-const assert = require('chai').assert;
-const expect = require('chai').expect
+const chai = require('chai')
+const spies = require('chai-spies');
+const chai_as_promised = require('chai-as-promised')
+chai.use(spies);
+chai.use(chai_as_promised);
+const assert = chai.assert;
+const expect = chai.expect;
 
 let HotClipsControllerClass = require('../lib/HotClipsController');
 let HotClipsController;
+let MonitorTwitchChatClass = require('../lib/MonitorTwitchChat');
 
 describe('HotClipsController methods', function() {
     before(function (){
         HotClipsController = new HotClipsControllerClass();
     })
+    describe('MonitorTwitchChat', function() {
+        it('should be a MonitorTwitchChat class', function() {
+            expect(HotClipsController.monitorTwitchChat).to.be.an.instanceOf(MonitorTwitchChatClass);
+        });
+    });
     describe('Get list', function() {
         it('should return an array', function() {
             assert.isArray(HotClipsController.getList());

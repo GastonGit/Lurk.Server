13193e8ebd750f7c240b66180e39d94f3bb7745f
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-01 13:59:57 +0200

    fetching index returns an array of twitch clips

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,14 @@ let router = express.Router();
 
 router.get('/', async (req ,res ,next) => {
   
-  return res.send("home");
+  return res.send([
+      'https://clips.twitch.tv/AgileStylishOilStinkyCheese',
+      'https://clips.twitch.tv/SparklySavageSnailBCWarrior',
+      'https://clips.twitch.tv/BitterCleverGoldfishEleGiggle--PW-MYiDBUXwTQw1',
+      'https://clips.twitch.tv/PerfectTawdrySproutFloof',
+      'https://clips.twitch.tv/PeacefulBlatantNewt4Head-gac5plWZtJ7LUNfz',
+      'https://clips.twitch.tv/QuaintAthleticVelociraptorPrimeMe-lX8gUVbZ7wvZFlCQ'
+  ]);
 });
 
 module.exports = router;

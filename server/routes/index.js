ecc38e7dece6a1cf5f15aba092a432a1c694376d
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-09 15:48:28 +0200

    enable index to get actual list of clips

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,14 +8,7 @@ hotClips.start();
 
 router.get('/', async (req ,res ,next) => {
   
-  return res.send([
-      'https://clips.twitch.tv/AgileStylishOilStinkyCheese',
-      'https://clips.twitch.tv/SparklySavageSnailBCWarrior',
-      'https://clips.twitch.tv/BitterCleverGoldfishEleGiggle--PW-MYiDBUXwTQw1',
-      'https://clips.twitch.tv/PerfectTawdrySproutFloof',
-      'https://clips.twitch.tv/PeacefulBlatantNewt4Head-gac5plWZtJ7LUNfz',
-      'https://clips.twitch.tv/QuaintAthleticVelociraptorPrimeMe-lX8gUVbZ7wvZFlCQ'
-  ]);
+  return res.send(hotClips.getList());
 });
 
 module.exports = router;

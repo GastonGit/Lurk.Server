d3d0ff22e228bb8d6438b3f007166cc3487408ad
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-20 18:09:42 +0200

    embed the first twitch clip from fetched from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {clips: []};
+  }
+
+  async fetchClips() {
+    fetch("http://localhost:9000/")
+        .then(res => res.json())
+        .then(data => {
+          let clips = []
+          data.forEach(function(clip){
+            clips.push('https://clips.twitch.tv/embed?clip=' + clip.substring(clip.indexOf('.tv')+4))
+          })
+          this.setState({clips: clips});
+        })
+        .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.fetchClips().catch(err => err);
+  }
+
+  render() {
+    if (!this.state.clips) {
+      return <span>Loading...</span>;
+    }
+
+    let clip = this.state.clips[0] + '&autoplay=true&parent=localhost';
+
+    return (
+        <div className="App">
+          <iframe
+              title="clip1"
+              className="clip-iframe"
+              src={clip}
+              frameBorder="0" allowFullScreen="{true}" scrolling="no" height="400" width="620"
+          />
+        </div>
+    );
+  }
 }
-
-export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { act } from "react-dom/test-utils";
 import App from './App';
+import {render, unmountComponentAtNode} from "react-dom";
+import React from 'react';
 
-test('does nothing', async () => {
+describe("App", () => {
+    let container = null;
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.appendChild(container);
+    });
+    afterEach(() => {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    });
+    describe("Fetching", () => {
+        it("should fetch clips and show them in the iframe", async () => {
+            const clips = ['https://clips.twitch.tv/SpunkySecretiveOrangeShadyLulu-KCNPm3bm3KTbuOCl'];
+            jest.spyOn(global, "fetch").mockImplementation(() =>
+                Promise.resolve({
+                    json: () => Promise.resolve(clips)
+                })
+            );
 
+            await act(async () => {
+                render(<App/>, container);
+            });
+
+            expect(
+                container.querySelector(".clip-iframe").getAttribute("src")
+            ).toEqual("https://clips.twitch.tv/embed?clip=SpunkySecretiveOrangeShadyLulu-KCNPm3bm3KTbuOCl&autoplay=true&parent=localhost");
+
+            global.fetch.mockRestore();
+        });
+    });
 });

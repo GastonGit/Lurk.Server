722c01f59e73b50c55a691d79cce77543a73bc42
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-22 22:13:39 +0200

    add buttons for navigating between old and new clips

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {clips: []};
+    this.state = {clips: [], currentClip: "", currentIt: 0};
+    this.nextClip = this.nextClip.bind(this);
+    this.prevClip = this.prevClip.bind(this);
   }
 
   async fetchClips() {
@@ -17,8 +19,27 @@ export default class App extends React.Component {
             clips.push('https://clips.twitch.tv/embed?clip=' + clip.substring(clip.indexOf('.tv')+4))
           })
           this.setState({clips: clips});
+          this.setState({currentClip: this.state.clips[0] + '&autoplay=true&parent=localhost'});
         })
         .catch(err => err);
+  }
+
+  nextClip(){
+      if (this.state.currentIt < this.state.clips.length){
+          this.setState((state) => {
+              return {currentIt: state.currentIt + 1}
+          });
+          this.setState({currentClip: this.state.clips[this.state.currentIt] + '&autoplay=true&parent=localhost'})
+      }
+  }
+
+  prevClip(){
+    if (this.state.currentIt > 0){
+        this.setState((state) => {
+            return {currentIt: state.currentIt - 1}
+        });
+        this.setState({currentClip: this.state.clips[this.state.currentIt] + '&autoplay=true&parent=localhost'})
+    }
   }
 
   componentDidMount() {
@@ -30,16 +51,20 @@ export default class App extends React.Component {
       return <span>Loading...</span>;
     }
 
-    let clip = this.state.clips[0] + '&autoplay=true&parent=localhost';
-
     return (
         <div className="App">
+            <button className="navButton" onClick={this.prevClip}>
+                Previous
+            </button>
           <iframe
               title="clip1"
               className="clip-iframe"
-              src={clip}
+              src={this.state.currentClip}
               frameBorder="0" allowFullScreen="{true}" scrolling="no" height="720" width="1280"
           />
+            <button className="navButton" onClick={this.nextClip}>
+                Next
+            </button>
         </div>
     );
   }

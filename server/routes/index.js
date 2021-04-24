d8195de0c198867ededfa857561571e2d76aa483
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-24 12:04:34 +0200

    re-enable index returning actual list of clips

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,13 +8,7 @@ hotClips.start();
 
 router.get('/', async (req ,res ,next) => {
   
-  // return res.send(hotClips.getList());
-  return res.send([
-      'https://clips-media-assets2.twitch.tv/AT-cm%7C1140679825.mp4',
-      'https://clips-media-assets2.twitch.tv/AT-cm%7C1147897618.mp4',
-      'https://clips-media-assets2.twitch.tv/AT-cm%7C1147927108.mp4',
-      'https://clips-media-assets2.twitch.tv/AT-cm%7C1148212903.mp4'
-  ]);
+  return res.send(hotClips.getList());
 });
 
 module.exports = router;

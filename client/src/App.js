16dfe3b1bfac11a240e9e2b1cfd350bec9cf58aa
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-04-24 16:34:51 +0200

    automatically update the list of clips and only show unshown clips

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {clips: [], currentClip: "", currentIt: -1};
+    this.state = {addedClips: [], clips: [], currentClip: "", noClips: false};
     this.nextClip = this.nextClip.bind(this);
   }
 
@@ -17,29 +17,73 @@ export default class App extends React.Component {
           data.forEach(function(clip){
             clips.push(clip);
           })
-          this.setState({clips: clips});
-          this.nextClip();
+          this.setState({clips: [...clips], addedClips: [...clips]});
+
+          if (clips.length === 0){
+              this.setState({noClips: true});
+          } else {
+              this.nextClip();
+          }
         })
         .catch(err => err);
   }
 
-  nextClip(){
-      if (this.state.currentIt < this.state.clips.length){
-          this.setState((state) => {
-              return {currentIt: state.currentIt + 1}
-          });
-          this.setState({currentClip: this.state.clips[this.state.currentIt]})
-      }
+  updateList(){
+      fetch("http://localhost:9000/")
+          .then(res => res.json())
+          .then(data => {
+              let newClips = [...data]
+              for (let i = 0; i < newClips.length; i++){
+                  if (!this.state.addedClips.includes(newClips[i])){
+                      let clips = [...this.state.clips];
+                      clips.push(newClips[i]);
+
+                      let addedClips = [...this.state.addedClips];
+                      addedClips.push(newClips[i]);
+
+                      this.setState({
+                          clips: clips,
+                          addedClips: addedClips
+                      });
+                  }
+              }
+
+              if (this.state.noClips){
+                  this.setState({noClips: false})
+                  this.nextClip();
+              }
+          })
+          .catch(err => err);
   }
+
+    nextClip(){
+      if (this.state.clips.length > 0){
+          let updatedArray = [...this.state.clips];
+          let currentClip = updatedArray.shift();
+          this.setState({
+              currentClip: currentClip,
+              clips: updatedArray
+          })
+      } else {
+          this.setState({noClips: true})
+      }
+    }
 
   componentDidMount() {
     this.fetchClips().catch(err => err);
     document.querySelector(".videoClip").onended = function() {
           this.nextClip();
       }.bind(this);
+
+    let updateInterval = setInterval(this.updateList.bind(this), 5000)
+    this.setState({updateInterval: updateInterval});
   }
 
-  render() {
+  componentWillUnmount(){
+      clearInterval(this.state.updateInterval);
+  }
+
+    render() {
     if (!this.state.clips) {
       return <span>Loading...</span>;
     }

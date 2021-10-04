3c5a802950924ce5f9e25ad1523b3f408bd3009a
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-10-04 14:07:53 +0200

    convert Footer.js to tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/Footer.css';
 
-export default class Footer extends React.Component {
-    constructor(props) {
+export default class Footer extends React.Component<unknown, unknown> {
+    constructor(props: unknown) {
         super(props);
         this.state = {};
     }
 
-    render() {
+    render(): JSX.Element {
         return (
             <footer className="Footer">
                 <a href="https://github.com/GastonGit/Hot-Twitch-Clips">

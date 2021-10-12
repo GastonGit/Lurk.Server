6294d8a15ea1ce94e3e113d74871ef036e5aaa9d
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-10-12 14:13:07 +0200

    setup header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from './components/Header';
 import App from './components/App';
 import Footer from './components/Footer';
+import Grid from '@mui/material/Grid';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
-        <Footer />
+        <Grid container className="RootGrid" direction="column" spacing={0}>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Header />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <App />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Footer />
+            </Grid>
+        </Grid>
     </React.StrictMode>,
     document.getElementById('root'),
 );

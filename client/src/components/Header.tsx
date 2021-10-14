afabe6a12ddd333f8c2d7f2256119f2c20ddf6f5
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-10-14 15:41:28 +0200

    center header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,20 +4,16 @@ import '../styles/Header.css';
 export default class Header extends React.Component<unknown, unknown> {
     render(): JSX.Element {
         return (
-            <div>
-                <header className="Head">
-                    <div className="HeaderContent">
-                        <div className="HeaderContentMain">
-                            <a
-                                href="https://gastongit.com/hotclips"
-                                className="Header-link"
-                            >
-                                Hot Twitch Clips
-                            </a>
-                        </div>
-                    </div>
-                </header>
-            </div>
+            <header className="Head">
+                <div className="HeaderContent">
+                    <a
+                        href="https://gastongit.com/hotclips"
+                        className="Header-link"
+                    >
+                        Hot Twitch Clips
+                    </a>
+                </div>
+            </header>
         );
     }
 }

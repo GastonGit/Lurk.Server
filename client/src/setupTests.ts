8abe3998d28346ca31034ab55499e541aa094c27
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-10-30 14:38:43 +0200

    fix test flush error

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Fixes unstable_flushDiscreteUpdates error caused by having a muted video element
+Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    set: () => {},
+});

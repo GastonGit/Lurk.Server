79ccd41fad293b67b598be42be6ae46c3b145217
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-18 18:11:27 +0100

    enable hotclips in router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,16 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-//import HotClipsController from '../../lib/HotClipsController';
+import HotClipsController from '../../lib/HotClipsController';
 import { Router, Request, Response } from 'express';
 const indexRouter = Router();
 
-// const hotClips = new HotClipsController();
-// hotClips.setupConnection();
-// hotClips.start();
+const hotClips = new HotClipsController();
+hotClips.setupConnection();
+hotClips.start();
 
 indexRouter.get('/', (req: Request, res: Response) => {
-    // res.send(hotClips.getList());
-    res.send('Boiler!');
+    res.send(hotClips.getList());
 });
 
 export default indexRouter;

b0b4c93e5d9d3dd80db97f0597fbbba2348a3a83
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-18 17:05:09 +0100

    create Fetcher class and replace node-fetch usages in MonitorTwitchChat with calls to Fetcher

---
+++ server/lib/Fetcher.js
@@ -0,0 +1,35 @@
+const fetch = require("node-fetch");
+
+export async function validAppAccessToken() {
+  const url = "https://api.twitch.tv/helix/users?id=141981764";
+
+  const response = await fetch(url, {
+    method: "get",
+    headers: {
+      "Client-ID": process.env.CLIENT_ID,
+      Authorization: " Bearer " + process.env.CLIENT_APP_ACCESS_TOKEN,
+    },
+  });
+
+  const status = await response.status;
+
+  return status !== 401;
+}
+
+export async function request100Streams(pagination) {
+  let url = "https://api.twitch.tv/helix/streams?first=100&language=en";
+
+  if (pagination) {
+    url += "&after=" + pagination;
+  }
+
+  const response = await fetch(url, {
+    method: "get",
+    headers: {
+      "Client-ID": process.env.CLIENT_ID,
+      Authorization: " Bearer " + process.env.CLIENT_APP_ACCESS_TOKEN,
+    },
+  });
+
+  return await response.json();
+}

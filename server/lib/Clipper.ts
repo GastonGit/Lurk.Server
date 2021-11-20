e2ba914d700d90de2f3938cb954e911bb475c1f8
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-20 14:38:48 +0100

    convert Clipper to TS

---
+++ server/lib/Clipper.ts
@@ -0,0 +1,174 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '../.env' });
+import fetch from 'node-fetch';
+
+interface Credentials {
+    id: string;
+    secret: string;
+    code: string;
+    refresh: string;
+}
+
+export default class Clipper {
+    private credentials: Credentials;
+
+    constructor() {
+        this.credentials = {
+            id: process.env.CLIENT_ID || '',
+            secret: process.env.CLIENT_SECRET || '',
+            code: process.env.CLIENT_CODE || '',
+            refresh: process.env.CLIENT_REFRESH || '',
+        };
+    }
+
+    async createClip(streamer: string): Promise<unknown> {
+        if (process.env.NODE_ENV === 'development') {
+            console.log('CLIP CREATED FOR: ' + streamer);
+            return { id: 'HealthyDelightfulEchidnaKappaPride' };
+        }
+
+        const broadcasterID: string = await this.getBroadcasterID(streamer);
+
+        const url: string =
+            'https://api.twitch.tv/helix/clips?broadcaster_id=' +
+            broadcasterID.toLowerCase();
+        const accessToken: string = await this.getAccessToken();
+
+        const response = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Client-ID': this.credentials.id,
+                Authorization: 'Bearer ' + accessToken,
+            },
+        });
+
+        const status = await response.status;
+
+        if (status === 200 || status === 202) {
+            const json: any = await response.json();
+            const data = json.data[0];
+
+            console.log(
+                '\x1b[32m%s\x1b[0m',
+                'createClip :: SUCCESS :: ' + streamer,
+            );
+            return { created: true, data };
+        } else {
+            console.log(
+                '\x1b[45m%s\x1b[0m',
+                'createClip :: FAILURE :: ' +
+                    streamer +
+                    ' (status code ' +
+                    status +
+                    ')',
+            );
+            return { created: false };
+        }
+    }
+
+    async getClip(slug: string): Promise<any> {
+        if (process.env.NODE_ENV === 'development') {
+            return {
+                thumbnail_url:
+                    'https://clips-media-assets2.twitch.tv/AT-cm%7C1140679825-preview-480x272.jpg',
+            };
+        }
+
+        const url = 'https://api.twitch.tv/helix/clips?id=' + slug;
+        const accessToken = await this.getAccessToken();
+
+        const response = await fetch(url, {
+            method: 'get',
+            headers: {
+                'Client-ID': this.credentials.id,
+                Authorization: 'Bearer ' + accessToken,
+            },
+        });
+
+        const result: any = await response.json();
+
+        return result.data[0];
+    }
+
+    async getVideoUrl(slug: string): Promise<unknown> {
+        const clip = await this.getClip(slug);
+
+        if (typeof clip !== 'undefined') {
+            const url = this.formatVideoUrl(clip.thumbnail_url);
+
+            return { valid: true, url };
+        } else {
+            return { valid: false };
+        }
+    }
+
+    formatVideoUrl(thumbnail_url: string): string {
+        const videoID = thumbnail_url.substring(
+            thumbnail_url.indexOf('.tv/') + 4,
+            thumbnail_url.indexOf('-preview'),
+        );
+
+        return 'https://clips-media-assets2.twitch.tv/' + videoID + '.mp4';
+    }
+
+    async getAccessToken(): Promise<string> {
+        if (process.env.NODE_ENV === 'development') {
+            return 'dev_string';
+        }
+        const url =
+            'https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=' +
+            this.credentials.refresh +
+            '&client_id=' +
+            this.credentials.id +
+            '&client_secret=' +
+            this.credentials.secret;
+
+        const response = await fetch(url, {
+            method: 'post',
+        });
+
+        const status = await response.status;
+
+        if (status !== 200) {
+            throw new Error('getAccessToken - status code is: ' + status);
+        }
+
+        const json: any = await response.json();
+
+        return json.access_token;
+    }
+
+    async getBroadcasterID(id: string): Promise<string> {
+        const user: any = await this.getUser(id);
+
+        return user.data[0].id;
+    }
+
+    async getUser(name: string): Promise<unknown> {
+        if (process.env.NODE_ENV === 'development') {
+            return { data: ['shit'] };
+        }
+
+        const url =
+            'https://api.twitch.tv/helix/users?' +
+            'login=' +
+            name.toLowerCase();
+        const accessToken = await this.getAccessToken();
+
+        const response = await fetch(url, {
+            method: 'get',
+            headers: {
+                'Client-ID': this.credentials.id,
+                Authorization: 'Bearer ' + accessToken,
+            },
+        });
+
+        const status = await response.status;
+
+        if (status !== 200) {
+            throw new Error('getUser - status code is: ' + status);
+        }
+
+        return await response.json();
+    }
+}

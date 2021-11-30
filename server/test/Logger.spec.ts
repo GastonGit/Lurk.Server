bc90aab0c9126c4f3e6b00f1d593d3e0e24b6c26
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-30 16:56:55 +0100

    install sinon with types and disable console output for Logger's test suite

--- server/test/Logger.spec.ts
+++ server/test/Logger.spec.ts
@@ -1,7 +1,14 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import Logger from '../lib/Logger';
 
 describe('Logger suite', function () {
+    before(function () {
+        sinon.stub(console, 'log');
+        sinon.stub(console, 'info');
+        sinon.stub(console, 'warn');
+        sinon.stub(console, 'error');
+    });
     describe('Calling methods with proper arguments', function () {
         it('should not throw', function () {
             expect(function () {

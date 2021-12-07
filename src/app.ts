69f7e45c15d2da2cf27d8e03c4c972c08e6a29ad
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-12-07 13:58:18 +0100

    setup cors and add domainOrigin to configs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application } from 'express';
+import config from 'config';
 import routes from './routes';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(helmet());
+app.use(
+    cors({
+        origin: config.get('domainOrigin'),
+    }),
+);
 app.use(express.json());
 app.use(routes);
 

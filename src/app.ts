032c106eed8a4a0b27d8bbc8d3abc7ef409a208b
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-12-07 13:59:34 +0100

    remove legacy comment from app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,31 +19,3 @@ app.use(routes);
 app.listen(PORT, (): void => {
     console.log(`Server Running at http://localhost:${PORT}`);
 });
-
-/*
-
-require('dotenv').config();
-
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let cors = require('cors');
-
-let indexRouter = require('./routes');
-
-let app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-module.exports = app;
-
- */
